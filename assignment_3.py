0533198c06b5a4173342699deb72cd7fe50376e9
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-05-28 17:18:54 +0200

    finished ass3

--- assignment_3.py
+++ assignment_3.py
@@ -1,11 +1,18 @@
-
+import math
 #fuction to get absolute vaue of nombre
 
 def absolute(number):
-	return sqrt(pow(number, 2))
+	#return math.sqrt(pow(number, 2))
+	if number < 0:
+		return -number
+	else:
+		return number
 
+#print(absolute(-3))
 #returns highest number of list of 
 def maximum(numberlist):
 	numberlist.sort()
-	#return numberlist[-1]
-	numberlist.pop(-1)
+	return numberlist[-1]
+	#numberlist.pop(-1)
+
+#print(maximum([1, 2, 3, 4, 300]))

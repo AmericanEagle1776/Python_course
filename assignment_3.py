4f2a7bfde71ba1f28ae8cb99ce953e6802a2babb
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-06-17 15:49:24 +0200

    committing ass 3 and 4

--- assignment_3.py
+++ assignment_3.py
@@ -1,6 +1,8 @@
 import math
 #fuction to get absolute vaue of nombre
-
+print('prints absolute value of number')
+print('number is ', str('3'))
+print('solutions:')
 def absolute(number):
 	if number < 0:
 		return -number
@@ -8,20 +10,27 @@ def absolute(number):
 		return number
 
 def absolute2(number):
+	"""absolute value by square then squareroot"""
 	return math.sqrt(pow(number, 2))
 
 def absolute3(number):
-	abs(number)
+	return abs(number)
 
-#print(absolute(-3))
+print(absolute(-3))
+print(absolute2(-3))
+print(absolute3(-3))
+
 #returns highest number of list of 
+print('prints max of list')
+print('list is', str('[1, 2, 3, 4, 300]'))
+print('solutions:')
 def maximum(numberlist):
 	numberlist.sort()
 	return numberlist[-1]
 	#numberlist.pop(-1)
 
 def maximum2(numberlist):
-	"""get max of lis"""
+	"""get max of list with for loop"""
 	max = numberlist[0]
 	for x in numberlist:
 		if x > max:
@@ -31,5 +40,7 @@ def maximum2(numberlist):
 def maximum3(numberlist):
 	return max(numberlist)
 
-#print(maximum([1, 2, 3, 4, 300]))
+print(maximum([1, 2, 3, 4, 300]))
+print(maximum2([1, 2, 3, 4, 300]))
+print(maximum3([1, 2, 3, 4, 300]))
 #print(maximum2([1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 2]))
--- assignment_4.py
+++ assignment_4.py
@@ -1,45 +1,79 @@
+import random
 class RockPaperScissors:
     """
     Implementation of the game Rock, Paper, Scissors
     """
-    def __init__(self):
+    def __init__(self): #i will need these variables
+        self.computerchoice = None
+        self.humanchoice = None
+        self.paper = {'rock': True, 'scissors' : False }
+        self.rock = {'paper': True, 'scissors' : False}
+        self.scissors = {'paper' : True, 'rock': False}
+        self.choice = {'paper' : self.paper, 'rock' : self.rock, 'scissors': self.scissors}
+        self.wincounter = 0
+        self.losscounter = 0
+        self.playagain = None
         pass
 
     def computer_choice(self):
         """
         The computer chooses a value
         """
-        pass
+        self.computerchoice = random.choice(['rock', 'paper', 'scissors'])
+        return self.computerchoice
 
     def human_choice(self):
         """
         The human player chooses a value
         """
-        pass
+        while answer not in choices:
+            self.humanchoice = input("input 'rock' paper' or 'scissors")
+
+        return self.humanchoice
+
 
     def evaluate_game(self):
         """
         Evaluate the values of the two players
         """
-        pass
+        if self.computerchoice == self.humanchoice :
+            print('tied game')
 
-    def print_result(self):
-        """
-        Visualise game result
-        """
-        pass
+        else:
+            if self.choice[self.humanchoice][self.computerchoice]:
+                print('win')
+                self.wincounter +=1
+                return self.wincounter
+            else:
+                print('loss')
+                self.losscounterounter += 1
+                return self.losscounter
 
-    def count_points(self):
-        """
-        Count the points of all rounds
-        """
-        pass
 
+ ####  def print_result(self):
+ ##      """
+ ##      Visualise game result
+ ##      """
+ ##
+ ##      pass
+
+ #   def count_points(self):
+ #       """
+ #       Count the points of all rounds
+ #       """
+ #       pass
+#
     def new_game_question(self):
         """
         Ask human player for new game
         """
-        pass
+        print("you have won", self.wincounter,  "times and lost", self.losscounter, "times" )
+        self.playagain = input("would you like to play again [y/n]")
+
+        if self.playagain:
+            return RockPaperScissors()
+        else:
+            pass
 
 
 if __name__ == "__main__":
